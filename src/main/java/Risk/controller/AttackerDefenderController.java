cad86bf366db3c1aa7f689e841c092b394d55451
jacksob1 <jacksob1@rose-hulman.edu>
2022-03-24 20:04:45 -0400

    extracted methods from combat method
    also called the sort method from the rollDice method instead of immediately after

--- src/main/java/Risk/controller/AttackerDefenderController.java
+++ src/main/java/Risk/controller/AttackerDefenderController.java
@@ -16,27 +16,22 @@ public class AttackerDefenderController {
 	int defenderArmies;
 
 	public void setAttacker(Player player) {
-		// TODO Auto-generated method stub
 		attacker = player;
 	}
 
 	public Player getAttacker() {
-		// TODO Auto-generated method stub
 		return attacker;
 	}
 
 	public void setDefender(Player player) {
-		// TODO Auto-generated method stub
 		defender = player;
 	}
 
 	public Player getDefender() {
-		// TODO Auto-generated method stub
 		return defender;
 	}
 
 	public Integer[] rollDice(int numDice, Random rand) {
-		// TODO Auto-generated method stub
 		if (numDice <= 0) {
 			throw new IllegalArgumentException("Incorrect Amount of Dice rolled");
 		}
@@ -44,92 +39,81 @@ public class AttackerDefenderController {
 		for (int i = 0; i < numDice; i++) {
 			toReturn[i] = rand.nextInt(6);
 		}
+
+		sortDice(toReturn);
 		return toReturn;
 	}
 
 	public void setAttackingTerritory(Territory t) {
-		// TODO Auto-generated method stub
 		if (attacker.getId() != t.getPlayer()) {
 			throw new IllegalArgumentException("Player does not own this territory " + attacker.getId() + " is the attacker " + t.getPlayer() + " is the owner");
 		}
 		attackerTerritory = t;
-		
 	}
 
 	public Territory getAttackingTerritory() {
-		// TODO Auto-generated method stub
 		return attackerTerritory;
 	}
 
 	public Territory getDefendingTerritory() {
-		// TODO Auto-generated method stub
 		return defenderTerritory;
 	}
 
 	public void setDefendingTerritory(Territory t) {
-		// TODO Auto-generated method stub
 		defenderTerritory = t;
-		
 	}
 
 	public void setArmies(int i) {
-		// TODO Auto-generated method stub
 		if (attackerTerritory.getArmyCount() - i < 2) {
 			throw new IllegalArgumentException("need a minimum of 2 armies left in attacking territory");
 		}
 		this.attackerArmies = i;
 		this.attackerTerritory.removeArmies(i);
 		this.defenderArmies = defenderTerritory.getArmyCount();
-		
 	}
 
 	public void combat(int i, int j, Random rand) {
-		// TODO Auto-generated method stub
 		if (i > attackerArmies || j > defenderArmies) {
 			throw new IllegalArgumentException("Must have dice equal to or less than the amount of armies");
 		}
 		
 		Integer[] attackerDice = rollDice(i, rand);
 		Integer[] defenderDice = rollDice(j, rand);
-		Arrays.sort(attackerDice, Collections.reverseOrder());
-		Arrays.sort(defenderDice, Collections.reverseOrder());
-		if (attackerDice.length >= defenderDice.length) {
-			for (int k = 0; k < defenderDice.length; k++) {
-				if (attackerDice[k].compareTo(defenderDice[k]) <= 0) {
-					attackerArmies -= 1;
-				}
-				if (attackerDice[k].compareTo(defenderDice[k]) > 0) {
-					defenderArmies -= 1;
-				}
-				
-				
-			}
-		}
-		if (attackerDice.length < defenderDice.length) {
-			for (int k = 0; k < attackerDice.length; k++) {
-				if (attackerDice[k].compareTo(defenderDice[k]) <= 0) {
-					attackerArmies -= 1;
-				}
-				if (attackerDice[k].compareTo(defenderDice[k]) > 0) {
-					defenderArmies -= 1;
-				}
-				
-				
-			}
-		}
+
+		attack(attackerDice, defenderDice);
+
+		update();
+	}
+
+	private void update() {
 		if (defenderArmies == 0) {
-			defender.removeTerritory();
-			attacker.addTerritory();
-			attacker.caughtTerritory(true);
-			defenderTerritory.setArmyCount(attackerArmies);
-			defenderTerritory.setPlayer(attacker.getId());
+			defenderLoses();
 		} else {
 			defenderTerritory.setArmyCount(defenderArmies);
 			attackerTerritory.addArmies(attackerArmies);
 		}
-		
-		
 	}
-	
 
+	private void defenderLoses() {
+		defender.removeTerritory();
+		attacker.addTerritory();
+		attacker.caughtTerritory(true);
+		defenderTerritory.setArmyCount(attackerArmies);
+		defenderTerritory.setPlayer(attacker.getId());
+	}
+
+	private void attack(Integer[] attackerDice, Integer[] defenderDice) {
+		for (int k = 0; k < Math.min(attackerDice.length, defenderDice.length); k++) {
+			if (attackerDice[k].compareTo(defenderDice[k]) <= 0) {
+				attackerArmies -= 1;
+			}
+			if (attackerDice[k].compareTo(defenderDice[k]) > 0) {
+				defenderArmies -= 1;
+			}
+		}
+	}
+
+	private void sortDice(Integer[] dice) {
+		Arrays.sort(dice, Collections.reverseOrder());
+	}
 }
--- src/test/java/Risk/controller/AttackerDefenderControllerTests.java
+++ src/test/java/Risk/controller/AttackerDefenderControllerTests.java
@@ -45,8 +45,8 @@ public class AttackerDefenderControllerTests {
 
 		Integer[] actual = adc.rollDice(2, rand);
 		Integer[] expected = new Integer[3];
-		expected[0] = 3;
-		expected[1] = 4;
+		expected[0] = 4;
+		expected[1] = 3;
 
 		EasyMock.verify(rand);
 		assertEquals(expected[0], actual[0]);
@@ -66,9 +66,9 @@ public class AttackerDefenderControllerTests {
 
 		Integer[] actual = adc.rollDice(3, rand);
 		Integer[] expected = new Integer[3];
-		expected[0] = 3;
+		expected[0] = 6;
 		expected[1] = 4;
-		expected[2] = 6;
+		expected[2] = 3;
 
 		EasyMock.verify(rand);
 		assertEquals(expected[0], actual[0]);
