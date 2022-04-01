9fc61662457f98dcb2dfd971ad57ecf0902bef5b
ansarijrhit <ansarij@rose-hulman.edu>
2022-04-01 09:29:59 -0400

    Testing is getting there

--- src/main/java/Risk/controller/PhaseController.java
+++ src/main/java/Risk/controller/PhaseController.java
@@ -100,7 +100,6 @@ public class PhaseController {
 
         public void nextPhase() {
             phase = "assignment";
-
             playerController.addNewArmiestoPlayer();
             int toaddarmies = gameBoardController
                     .getNewContinentPlayerArmies(playerController.getCurrentPlayer().getId());
