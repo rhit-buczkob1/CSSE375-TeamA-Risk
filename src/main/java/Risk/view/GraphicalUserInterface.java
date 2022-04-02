bfe748d68559c038a819106a1a56f7d770631c93
Brock Buczkowski <buczkob1@rose-hulman.edu>
2022-04-01 22:43:27 -0400

    Finished converting GUI to javafx

--- src/main/java/Risk/view/GraphicalUserInterface.java
+++ src/main/java/Risk/view/GraphicalUserInterface.java
@@ -17,11 +17,15 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 import javax.swing.*;
+
+import Risk.model.Card;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -50,10 +54,6 @@ public class GraphicalUserInterface {
 	public int currentTerritoryPlayer = 0;
 	private ResourceBundle messages;
 	
-//	private int xShift = 9;
-//	private int yShift = 38;
-	
-	public GuiComponent component;
 	public Button nextTurn;
 	public Text currentPlayer;
 	public Text currentPlayerId = new Text("1");
@@ -63,19 +63,18 @@ public class GraphicalUserInterface {
 	public Text territoryArmiesNumber = new Text("0");
 	public Text territoryPlayer;
 	public Text territoryPlayerNumber = new Text("0");
-	public JButton attack;
+	public Button attack;
 	public Button language;
-	public JLabel info = new JLabel("");
 	public Button addArmy;
-	public JLabel attackerDice;
-	public JLabel defenderDice;
-	public JSlider attackerDiceSlider = new JSlider(1, 3, 1);
-	public JSlider defenderDiceSlider = new JSlider(1, 2, 1);
-	public JComboBox card1 = new JComboBox();
-	public JComboBox card2 = new JComboBox();
-	public JComboBox card3 = new JComboBox();
-	public JButton spendCards;
-	public String currentPhase = "Setup";
+	public Text attackerDice;
+	public Text defenderDice;
+	public Slider attackerDiceSlider = new Slider(1, 3, 1);
+	public Slider defenderDiceSlider = new Slider(1, 2, 1);
+	public ComboBox<String> card1 = new ComboBox<String>();
+	public ComboBox<String> card2 = new ComboBox<String>();
+	public ComboBox<String> card3 = new ComboBox<String>();
+	public Button spendCards;
+	public Text currentPhase = new Text("Setup");
 
 
 	public Text currentTerritoryDesc;
@@ -101,11 +100,11 @@ public class GraphicalUserInterface {
 		this.playerArmies = new Text(messages.getString("armiesInPlayersHands"));
 		this.territoryArmies = new Text(messages.getString("armiesTerr"));
 		this.territoryPlayer = new Text(messages.getString("terrPlayer"));
-		this.attack = new JButton(messages.getString("attack"));
+		this.attack = new Button(messages.getString("attack"));
 		this.addArmy = new Button(messages.getString("addArmy"));
-		this.attackerDice = new JLabel(messages.getString("selectAttDice"));
-		this.defenderDice = new JLabel(messages.getString("selectDefDice"));
-		this.spendCards = new JButton(messages.getString("spendCards"));
+		this.attackerDice = new Text(messages.getString("selectAttDice"));
+		this.defenderDice = new Text(messages.getString("selectDefDice"));
+		this.spendCards = new Button(messages.getString("spendCards"));
 	}
 	
 	public GraphicalUserInterface(ResourceBundle msg, Stage stage) {
@@ -128,11 +127,11 @@ public class GraphicalUserInterface {
 		this.playerArmies = new Text(messages.getString("armiesInPlayersHands"));
 		this.territoryArmies = new Text(messages.getString("armiesTerr"));
 		this.territoryPlayer = new Text(messages.getString("terrPlayer"));
-		this.attack = new JButton(messages.getString("attack"));
+		this.attack = new Button(messages.getString("attack"));
 		this.addArmy = new Button(messages.getString("addArmy"));
-		this.attackerDice = new JLabel(messages.getString("selectAttDice"));
-		this.defenderDice = new JLabel(messages.getString("selectDefDice"));
-		this.spendCards = new JButton(messages.getString("spendCards"));
+		this.attackerDice = new Text(messages.getString("selectAttDice"));
+		this.defenderDice = new Text(messages.getString("selectDefDice"));
+		this.spendCards = new Button(messages.getString("spendCards"));
 	}
 	
 	public void setLanguage(ResourceBundle msg, String phase) {
@@ -150,8 +149,7 @@ public class GraphicalUserInterface {
 		this.spendCards.setText(messages.getString("spendCards"));
 		this.language.setText(messages.getString("lang"));
 		this.stage.setTitle(messages.getString("risk"));
-		this.currentPhase = messages.getString(phase);
-//		this.component.repaint();
+		this.currentPhase.setText(messages.getString(phase));
 	}
 
 	public void setMouseListener(EventHandler<MouseEvent> listener) {
@@ -178,25 +176,43 @@ public class GraphicalUserInterface {
 		if (testMode) {
 			return;
 		}
-		JFrame f = new JFrame(); // creates jframe f
+		Stage victory = new Stage(); // creates jframe f
 
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // this is your screen size
-
-		f.setUndecorated(true); // removes the surrounding border
-
-		ImageIcon image = new ImageIcon("src/main/resources/victoryscreen.jpg"); //imports the image
-
-		JLabel lbl = new JLabel(image); // puts the image into a jlabel
-
-		f.getContentPane().add(lbl); // puts label inside the jframe
-
-		f.setSize(image.getIconWidth(), image.getIconHeight()); // gets h and w of image and sets jframe to the size
-
-		int x = (screenSize.width - f.getSize().width) / 2; // These two lines are the dimensions
-		int y = (screenSize.height - f.getSize().height) / 2; // of the center of the screen
-
-		f.setLocation(x, y); // sets the location of the jframe
-		f.setVisible(true); // makes the jframe visible
+		GridPane pane = new GridPane();
+		
+		try {
+			BackgroundImage bImg = new BackgroundImage(new Image(new FileInputStream("src/main/resources/victoryscreen.jpg")),
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundPosition.DEFAULT,
+                    BackgroundSize.DEFAULT);
+			
+			pane.setBackground(new Background(bImg));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		victory.setScene(new Scene(pane));
+		
+		victory.show();
+		
+//		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // this is your screen size
+//
+//		f.setUndecorated(true); // removes the surrounding border
+//
+//		ImageIcon image = new ImageIcon("src/main/resources/victoryscreen.jpg"); //imports the image
+//
+//		JLabel lbl = new JLabel(image); // puts the image into a jlabel
+//
+//		f.getContentPane().add(lbl); // puts label inside the jframe
+//
+//		f.setSize(image.getIconWidth(), image.getIconHeight()); // gets h and w of image and sets jframe to the size
+//
+//		int x = (screenSize.width - f.getSize().width) / 2; // These two lines are the dimensions
+//		int y = (screenSize.height - f.getSize().height) / 2; // of the center of the screen
+//
+//		f.setLocation(x, y); // sets the location of the jframe
+//		f.setVisible(true); // makes the jframe visible
 	}
 
 	/*
@@ -215,6 +231,9 @@ public class GraphicalUserInterface {
 			}
 		}
 		this.clickedIndex = -1;
+		clickedTerritory.setText("");
+		this.setCurrentTerritoryOwner(0);
+		this.setTerritoryArmyCount(0);
 		return "";
 	}
 
@@ -262,7 +281,6 @@ public class GraphicalUserInterface {
 		
 		GridPane pane = new GridPane();
 		
-		this.component = new GuiComponent(this.territoriesBounds);
 		try {
 			BackgroundImage bImg = new BackgroundImage(new Image(new FileInputStream("src/main/resources/background.png")),
                     BackgroundRepeat.NO_REPEAT,
@@ -285,20 +303,11 @@ public class GraphicalUserInterface {
 		
 		pane.setOnMouseClicked(this.mouseListener);
 		
-		JPanel currentPlayerPanel = new JPanel();
-		JPanel currentTerritoryAndCardsPanel = new JPanel();
-		
-		currentTerritoryAndCardsPanel.add(card1);
-		currentTerritoryAndCardsPanel.add(card2);
-		currentTerritoryAndCardsPanel.add(card3);
-		currentTerritoryAndCardsPanel.add(spendCards);
-		
-		currentPlayerPanel.setLayout(new GridLayout(2, 6));
-//		currentPlayerPanel.add(currentPlayer);
-		
 		GridPane options = new GridPane();
 		
 		Group player = new Group();
+		
+		player.getChildren().add(currentPhase);
 		
 		player.getChildren().add(currentPlayer);
 		player.getChildren().add(currentPlayerId);
@@ -313,6 +322,8 @@ public class GraphicalUserInterface {
 		player.getChildren().add(territoryArmiesNumber);
 		
 		Font standard = Font.font("Tahoma", FontWeight.NORMAL, 20);
+		
+		currentPhase.setFont(Font.font("Tahoma", FontWeight.BOLD, 25));
 		
 		currentPlayer.setFont(standard);
 		currentPlayerId.setFont(standard);
@@ -329,59 +340,80 @@ public class GraphicalUserInterface {
 		currentTerritoryDesc.setFont(standard);
 		clickedTerritory.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
 		
+		currentPlayer.setY(35);
+		currentPlayerId.setY(35);
 		currentPlayerId.setX(220);
 		
-		playerArmies.setY(35);
-		playerArmiesNumber.setY(35);
+		playerArmies.setY(70);
+		playerArmiesNumber.setY(70);
 		playerArmiesNumber.setX(300);
 		
-		currentTerritoryDesc.setY(70);
-		clickedTerritory.setY(105);
+		currentTerritoryDesc.setY(105);
+		clickedTerritory.setY(140);
 		
-		territoryPlayer.setY(150);
-		territoryPlayerNumber.setY(150);
+		territoryPlayer.setY(185);
+		territoryPlayerNumber.setY(185);
 		territoryPlayerNumber.setX(300);
 		
-		territoryArmies.setY(185);
-		territoryArmiesNumber.setY(185);
+		territoryArmies.setY(220);
+		territoryArmiesNumber.setY(220);
 		territoryArmiesNumber.setX(300);
-
-		currentPlayerPanel.add(attack);
 		
+		attackerDiceSlider.setSnapToTicks(true);
+		attackerDiceSlider.setMinorTickCount(0);
+		attackerDiceSlider.setMajorTickUnit(1);
+		attackerDiceSlider.setShowTickMarks(true);
+		attackerDiceSlider.setShowTickLabels(true);
 		
-		attackerDiceSlider.setMajorTickSpacing(1);
-		attackerDiceSlider.setMinorTickSpacing(1);
-		attackerDiceSlider.setPaintTicks(true);
-		attackerDiceSlider.setPaintLabels(true);
-		
-		defenderDiceSlider.setMajorTickSpacing(1);
-		defenderDiceSlider.setMinorTickSpacing(1);
-		defenderDiceSlider.setPaintTicks(true);
-		defenderDiceSlider.setPaintLabels(true);
-		
-		currentPlayerPanel.add(attackerDice);
-		currentPlayerPanel.add(attackerDiceSlider);
-
-		currentPlayerPanel.add(defenderDice);
-		currentPlayerPanel.add(defenderDiceSlider);
+		defenderDiceSlider.setSnapToTicks(true);
+		defenderDiceSlider.setMinorTickCount(0);
+		defenderDiceSlider.setMajorTickUnit(1);
+		defenderDiceSlider.setShowTickMarks(true);
+		defenderDiceSlider.setShowTickLabels(true);
 		
 		Group buttons = new Group();
 		
 		buttons.getChildren().add(addArmy);
 		buttons.getChildren().add(nextTurn);
 		buttons.getChildren().add(language);
+		buttons.getChildren().add(attack);
+		
+		buttons.getChildren().add(attackerDiceSlider);
+		buttons.getChildren().add(defenderDiceSlider);
+		buttons.getChildren().add(attackerDice);
+		buttons.getChildren().add(defenderDice);
 		
 		nextTurn.setTranslateY(30);
-		language.setTranslateY(60);
+		attack.setTranslateY(60);
+		language.setTranslateY(90);
 		
-		currentTerritoryAndCardsPanel.setLayout(new GridLayout(5, 1));
-//		currentTerritoryAndCardsPanel.add(language);
-		currentTerritoryAndCardsPanel.setPreferredSize(new Dimension((int) stage.getWidth() / 8, (int) stage.getHeight()));
+		attackerDice.setTranslateY(140);
+		defenderDice.setTranslateY(170);
+		attackerDiceSlider.setTranslateY(130);
+		defenderDiceSlider.setTranslateY(170);
+		attackerDiceSlider.setTranslateX(125);
+		defenderDiceSlider.setTranslateX(125);
+		
+		Group cards = new Group();
+		
+		cards.getChildren().add(card1);
+		cards.getChildren().add(card2);
+		cards.getChildren().add(card3);
+		cards.getChildren().add(spendCards);
+		
+		card1.setPrefWidth(100);
+		card2.setPrefWidth(100);
+		card3.setPrefWidth(100);
+		
+		card2.setTranslateX(100);
+		card3.setTranslateX(200);
+		spendCards.setTranslateY(150);
 		
 		pane.setAlignment(Pos.TOP_LEFT);
 		
 		options.add(player, 0, 0);
 		options.add(buttons, 0, 1);
+		options.add(cards, 0, 2);
 		
 		pane.add(root, 1, 1);
 		pane.add(options, 2, 1);
@@ -431,89 +463,6 @@ public class GraphicalUserInterface {
 		return rect;
 	}
 	
-	public class GuiComponent extends JComponent {
-
-//		/**
-//		 * Auto generated to prevent the waring from not having this.
-//		 */
-//		private static final long serialVersionUID = 1L;
-//		Image background;
-//		Image original;
-//		private ArrayList<Rectangle> territoryBounds;
-//
-		public GuiComponent(ArrayList<Rectangle> territoryBounds) {
-//			this.territoryBounds = (ArrayList<Rectangle>) territoryBounds.clone();
-//			try {
-//				this.original = ImageIO.read(new File("src/main/resources/background.png"));
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-		}
-
-		@Override
-		public void paint(Graphics g) {
-//			g.translate(-xShift, -yShift);
-//			this.paintBackgroundImage(g);
-//			this.paintTerritoryBounds(g);
-//			this.drawInfoScreen(g);
-//			g.translate(xShift, yShift);
-		}
-
-//		private void drawInfoScreen(Graphics g) {
-//			territoryArmiesNumber.setText(Integer.toString(currentTerritoryArmyCount));
-//			territoryPlayerNumber.setText(Integer.toString(currentTerritoryPlayer));
-//			java.awt.Color previousColor = g.getColor();
-//			g.setColor(Color.WHITE);
-//			g.setFont(new Font("TimesRoman", Font.PLAIN, (int) ((stage.getWidth()) / 50)));
-//			g.translate(getBackgroundWidth(), 0);
-//			g.drawString(currentPhase, 30, 100);
-//			g.translate(-getBackgroundWidth(), 0);
-//			g.setColor(previousColor);
-//		}
-//
-//		private void paintTerritoryBounds(Graphics g) {
-//			Color previous = g.getColor();
-//			Graphics2D g2 = (Graphics2D) g;
-//			currentBounds.clear();
-//			for (int i = 0; i < territoryBounds.size(); i++) {
-//				Rectangle2D.Double border = getTerritoryBox(i);
-//				border.width = border.width + 10;
-//				border.height = border.height + 10;
-//				border.x = border.x - 5;
-//				border.y = border.y - 5;
-//				
-//				Rectangle2D.Double rect = getTerritoryBox(i);
-//				
-//				g2.setColor(Color.DARK_GRAY);
-//				if (clickedIndex == i) {
-//					g2.setColor(Color.WHITE);
-//				}
-//				g2.fill(border);
-//				g2.setColor(territoryColors.get(i));
-//				g2.fill(rect);
-//			}
-//			g.setColor(previous);
-//		}
-//
-//		private Rectangle2D.Double getTerritoryBox(int pos) {
-//			return new Rectangle2D.Double(
-//					((double) territoryBounds.get(pos).x) * stage.getWidth() / screenWidth,
-//					((double) territoryBounds.get(pos).y) * stage.getHeight() / screenHeight,
-//					((double) stage.getWidth()) / 50, ((double) stage.getHeight()) / 25);
-//		}
-//
-//		private void paintBackgroundImage(Graphics g) {
-////			this.background = original.getScaledInstance(getBackgroundWidth(), (int) stage.getHeight(),
-////					Image.SCALE_DEFAULT);
-//			g.fillRect(0, 0, 10000, 10000);
-////			g.drawImage(background, 0, 0, null);
-//		}
-//		
-//		private int getBackgroundWidth() {
-//			return (int) (3 * stage.getWidth() / 4);
-//		}
-//	}
-	}
 	public Color getColorForPlayer(int player) {
 		if (player == 1) {
 			return Color.RED;
