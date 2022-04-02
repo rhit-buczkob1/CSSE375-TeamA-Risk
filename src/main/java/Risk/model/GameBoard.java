0044fe8f11873ecb6aad24c3478454888be5426f
jamari-morrison <RedCater360@yahoo.com>
2022-04-01 20:40:10 -0400

    fix compile issues

--- src/main/java/Risk/model/GameBoard.java
+++ src/main/java/Risk/model/GameBoard.java
@@ -87,7 +87,7 @@ public class GameBoard {
 	public Territory getTerritoryFromString(String name) {
 		for (Continent continent : this.continents) {
 			for (Territory territory : continent.territories) {
-				if (territory.name.equals(name)) {
+				if (territory.getName().equals(name)) {
 					return territory;
 				}
 			}
