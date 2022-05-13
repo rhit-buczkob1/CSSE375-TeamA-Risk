1d7f23da77a7a8d4a8d9ac15cef6f70e3e7b93ea
ansarijrhit <ansarij@rose-hulman.edu>
2022-05-13 08:42:48 -0400

    Merged with main and stuff

--- src/test/java/Risk/controller/IntegrationTests.java
+++ src/test/java/Risk/controller/IntegrationTests.java
@@ -1,5 +1,39 @@
 package Risk.controller;
 
-public class IntegrationTests {
+import Risk.view.GraphicalUserInterface;
+import javafx.stage.Stage;
+import org.junit.BeforeClass;
+import org.junit.*;
+import org.testfx.framework.junit.ApplicationTest;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class IntegrationTests extends ApplicationTest {
+    Locale locale = new Locale("en", "US");
+    ResourceBundle msg = ResourceBundle.getBundle("MessagesBundle", locale);
+
+    GraphicalUserInterface gui;
+
+//    @BeforeClass
+//    public static void headless() {
+//        System.setProperty("java.awt.headless", "true");
+//        System.setProperty("testfx.robot", "glass");
+//        System.setProperty("testfx.headless", "true");
+//        System.setProperty("glass.platform", "Monocle");
+//        System.setProperty("monocle.platform", "Headless");
+//        System.setProperty("prism.order", "sw");
+//        System.setProperty("prism.text", "t2k");
+//        System.setProperty("testfx.setup.timeout", "2500");
+//    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        gui = new GraphicalUserInterface(msg, stage);
+    }
+
+    @Test
+    public void testButtonResponsiveness() {
+        assert(true);
+    }
 }
